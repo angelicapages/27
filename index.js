00151a12bb985245a463d51f18e5645081d4fe4a
angelicapages <angelicapages@gmail.com>
2020-09-29 13:44:13 -0300

    Ahora funciona todo en conjunto

--- index.js
+++ index.js
@@ -14,33 +14,46 @@ const filtroColor = document.querySelectorAll("input[type='checkbox']");
 // ver si el nombre escrit en el input coincide con el data-nombre de la tarjeta // true / false
 // ver si alguno de los checkbox chequeados coincide con los colores de la tarjeta // true / false
 // ver si el radio chequeado coincide con el sexo de la tarjeta // true / false 
-
-
-const pasaFiltros = (card) => {
-
   // me fijo si hay algo escrito en el input, 
   // si hay algo escrito en el input me fijo si lo escrito coincide con la tarjeta
   // si coincide con lo escrito en la trajeta retorno true 
   // si no coincide, retorno false 
 
-  // if (hayAlgunCheckBoxChequeado()) {
-  //   if (compararCheckBoxChequeado(card)) {
-  //     console.log("Pasa filtros")
-  //     return true
-  //   }
-  //   else {
-  //     return false
-  //   }
-  // }
-  // else {
-  //   return true
-  // }
+const pasaFiltrosCheckBox = (card) => {
+  if (hayAlgunCheckBoxChequeado()) {
+    if (compararCheckBoxChequeado(card)) {
+      console.log("Pasa filtros")
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  else {
+    return true
+  }
+}
 
+const pasaFiltrosRadio = (card) => {
   if (hayAlgunRadioChequeado()) {
     if (compararRadioChequeado(card)) {
       console.log ("pasa filtros")
       return true
+  
+    }
+    else {
+      return false
+    }
+  }
+  else {
+    return true
+  }
+}
 
+const pasaFiltrosInput = (card) => {
+  if (hayAlgoEscritoEnElInput()) {
+    if (compararInputConTarjeta(card)) {
+      return true
     }
     else {
       return false
@@ -52,19 +65,16 @@ const pasaFiltros = (card) => {
 }
 
 
-
-//   if (hayAlgoEscritoEnElInput()) {
-//     if (compararInputConTarjeta(card)) {
-//       return true
-//     }
-//     else {
-//       return false
-//     }
-//   }
-//   else {
-//     return true
-//   }
-
+const pasaFiltrosTodos = (card) => {
+if (pasaFiltrosCheckBox(card) && pasaFiltrosInput(card) && pasaFiltrosRadio(card)){
+console.log ("hola pepo")
+return true
+}
+else {
+  console.log("no")
+  return false
+}
+}
 // me fijo si hay algo chequeado en los checkbox
 // si lo hay, me fijo que checkbox esta chequeado
 // si los checkbox chequeados coinciden con el color de alguna tarjeta
@@ -79,50 +89,31 @@ const pasaFiltros = (card) => {
 // retorno true, sino retorno false 
 // }
 
-const ocultarTarjeta = (card) => {
-  return card.classList.add("hidden")
-}
 
-const mostrarTarjeta = (card) => {
-  return card.classList.remove("hidden")
-}
-
-const filtrarTarjetas = () => {
-  for (let card of cards) {
-    if (pasaFiltros(card)) {
-      mostrarTarjeta(card)
-    }
-    else {
-      ocultarTarjeta(card)
-    }
+const compararInputConTarjeta = (card) => {
+  if (card.dataset.nombre.includes(filtroBusqueda.value.toLowerCase())) {
+    return true
+  }
+  else {
+    return false
   }
 }
 
-
-// const compararInputConTarjeta = (card) => {
-//   if (card.dataset.nombre.includes(filtroBusqueda.value.toLowerCase())) {
-//     return true
-//   }
-//   else {
-//     return false
-//   }
-// }
-
-// const compararCheckBoxChequeado = (card) => {
-//   for (let checkbox of filtroColor) {
-//     if (checkbox.checked){
-//     if (checkbox.value === card.dataset.color) {
-//       console.log("hay uno que coincide")
-//       return true
-//     }
-//     else if (checkbox.value=== "todos"){
-//       console.log("todos los gatos")
-//       return true
-//     }
-//   }
-// }
-//   return false
-// }
+const compararCheckBoxChequeado = (card) => {
+  for (let checkbox of filtroColor) {
+    if (checkbox.checked){
+    if (checkbox.value === card.dataset.color) {
+      console.log("hay uno que coincide")
+      return true
+    }
+    else if (checkbox.value=== "todos"){
+      console.log("todos los gatos")
+      return true
+    }
+  }
+}
+  return false
+}
 const compararRadioChequeado = (card) => {
   for (let radio of filtroSexo) {
     if (radio.checked) {
@@ -139,16 +130,16 @@ const compararRadioChequeado = (card) => {
   return false
 }
 
-// const hayAlgunCheckBoxChequeado = () => {
-//   for (let checkbox of filtroColor) {
-//     if (checkbox.checked) {
-//       console.log("estoy chequeado")
-//       return true
-//     }
-//   }
-//   console.log("no está chequeado")
-//   return false
-// }
+const hayAlgunCheckBoxChequeado = () => {
+  for (let checkbox of filtroColor) {
+    if (checkbox.checked) {
+      console.log("estoy chequeado")
+      return true
+    }
+  }
+  console.log("no está chequeado")
+  return false
+}
 
 
 
@@ -161,29 +152,70 @@ const hayAlgunRadioChequeado = () => {
   return false
 }
 
-// const hayAlgoEscritoEnElInput = () => {
-//   if (filtroBusqueda.value) {
-//     return true 
-//   }
-//   else {
-//     return false
-//   }
-// }
-
-// filtroBusqueda.oninput = () => {
-//   filtrarTarjetas()
-
-// }
+const hayAlgoEscritoEnElInput = () => {
+  if (filtroBusqueda.value) {
+    return true 
+  }
+  else {
+    return false
+  }
+}
 
 
-// for (let checkbox of filtroColor) {
-//   checkbox.oninput = () => {
-//     filtrarTarjetas()
-//   }
-// }
+const ocultarTarjeta = (card) => {
+  return card.classList.add("hidden")
+}
+
+const mostrarTarjeta = (card) => {
+  return card.classList.remove("hidden")
+}
+
+const filtrarTarjetasPorInput = () => {
+  for (let card of cards) {
+    if (pasaFiltrosInput(card) && pasaFiltrosTodos(card)) {
+      mostrarTarjeta(card)
+    }
+    else {
+      ocultarTarjeta(card)
+    }
+  }
+}
+
+const filtrarTarjetasPorCheckbox = () => {
+  for (let card of cards) {
+    if (pasaFiltrosCheckBox(card) && pasaFiltrosTodos(card)) {
+      mostrarTarjeta(card)
+    }
+    else {
+      ocultarTarjeta(card)
+    }
+  }
+}
+
+const filtrarTarjetasPorRadio = () => {
+  for (let card of cards) {
+    if (pasaFiltrosRadio(card) && pasaFiltrosTodos(card)) {
+      mostrarTarjeta(card)
+    }
+    else {
+      ocultarTarjeta(card)
+    }
+  }
+}
+filtroBusqueda.oninput = () => {
+  filtrarTarjetasPorInput()
+
+}
+
+
+for (let checkbox of filtroColor) {
+  checkbox.oninput = () => {
+    filtrarTarjetasPorCheckbox()
+  }
+}
 
 for (let radio of filtroSexo) {
   radio.oninput = () => {
-    filtrarTarjetas()
+    filtrarTarjetasPorRadio()
   }
 }
